222ce06bcfdfc353befae72edc74d99b1f504f37
91955 <91955@LAPTOP-UJ64DTUR>
2023-01-10 11:03:06 +0530

    My second update

---
+++ Sonu_Mvn/src/test/java/p3/ClassC.java
@@ -0,0 +1,18 @@
+package p3;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ClassC 
+{
+	@Test
+	  public void tc007() 
+	  {
+		  Reporter.log("tc007 executed");
+	  }
+	  @Test
+	  public void tc008() 
+	  {
+		  Reporter.log("tc008 executed");
+	  }
+}
